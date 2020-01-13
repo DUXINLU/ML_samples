46d8b9f6d9dfd6d07d8afe0af2b278f6ef85af64
DUXINLU <duxinlu81@live.com>
2020-01-13 23:56:01 +0800

    鸢尾花逻辑回归

---
+++ iris_logisticregression/main.py
@@ -0,0 +1,18 @@
+# coding=UTF-8
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+import matplotlib as plt
+import numpy as np
+
+# load iris dataset
+X = load_iris().data
+y = load_iris().target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+LR_model = LogisticRegression(penalty='l2', solver='newton-cg', multi_class='multinomial', verbose=1)
+LR_model.fit(X_train, y_train)
+
+y_predict = LR_model.predict(X_test)
+accuracy = metrics.accuracy_score(y_test, y_predict)
+print(LR_model.score(X_train, y_train), LR_model.score(X_test, y_test), accuracy)
